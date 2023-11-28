95e093c7d7539f8b0e416f44a82e4bbd6eb401a7
oslab69 <142154133+oslab69@users.noreply.github.com>
2023-11-28 23:26:17 +0530

    Update Linked.c

--- Linked.c
+++ Linked.c
@@ -1,53 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define max 100
-int main()
-{
-    int f[max], n, val, ch, b, start;
-    for (int i = 0; i < max; i++)
-    {
-        f[i] = 0;
-    }
-    printf("Enter the number of blocks which are already allocated: \n");
-    scanf("%d", &n);
-    printf("Enter the blocks which are aready allocated: \n");
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d", &val);
-        f[val] = 1;
-    }
-a:
-    printf("1. add file 2. exit: \n");
-    scanf("%d", &ch);
-    switch (ch)
-    {
-    case 1:
-    {
-        printf("Enter the no of blocks to be allocated and starting block: \n");
-        scanf("%d%d", &b, &start);
-        if (f[start] == 1)
-        {
-            printf("Already alocated..");
-            goto a;
+
+struct node {
+    int data;
+    struct node *link;
+} *newNode, *first, *last, *temp;
+
+struct node memblock[25];
+
+int isAllocated(int start, int size) {
+    for (int i = start; i < start + size; i++) {
+        if (memblock[i].data == 1) {
+            return 1; // Memory block is already allocated
         }
-        for (int i = start; i < (start + b); i++)
-        {
-            if (f[i] == 0)
-            {
-                f[i] = 1;
-                printf("bno: %d\t---->%d\n", i, f[i]);
-            }
-            else
-            {
-                b++;
+    }
+    return 0; // Memory blocks are available
+}
+
+void linked() {
+    int start, size;
+    printf("Enter start block: ");
+    scanf("%d", &start);
+    printf("Enter size: ");
+    scanf("%d", &size);
+
+    int flag = isAllocated(start, size);
+
+    if (flag == 0) {
+        for (int i = start; i < start + size; i++) {
+            memblock[i].data = 1;
+
+            newNode = (struct node *)malloc(sizeof(struct node));
+            newNode->data = 1;
+            newNode->link = NULL;
+
+            if (first == NULL) {
+                first = newNode;
+                last = newNode;
+            } else {
+                last->link = newNode;
+                last = newNode;
             }
         }
-        goto a;
-        break;
+        printf("File allocated to blocks %d-%d\n", start, start + size - 1);
+    } else {
+        printf("Some blocks are already allocated.\n");
     }
-    case 2:
-    {
-        exit(0);
-    }
-    }
+}
+
+int main() {
+    int choice;
+
+    do {
+        printf("\n1. Linked\n2. Exit\n");
+        printf("Enter the choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                linked();
+                break;
+            case 2:
+                exit(0);
+            default:
+                printf("Try another option.\n");
+        }
+    } while (choice != 2);
+
+    return 0;
 }
