7cc3c8ca1f590296be1fbf31ba49f8934840bff4
oslab69 <142154133+oslab69@users.noreply.github.com>
2023-12-12 23:27:24 +0530

    Update lru.c

--- lru.c
+++ lru.c
@@ -1,83 +1,66 @@
-#include <stdio.h>
-
-void LRU(int[], int[], int[], int, int);
-int findLRU(int[], int);
-
-int main() {
-    int i, pCount, fCount, pages[30], frames[20], time[20];
-    printf("Number of Frames : ");
-    scanf("%d", &fCount);
-    // create frames array will null values
-    for (i = 0; i < fCount; ++i) {
-        frames[i] = -1;
-    }
-    printf("Number of Pages : ");
-    scanf("%d", &pCount);
-    printf("Enter the reference string\n");
-    for (i = 0; i < pCount; ++i) {
-        scanf("%d", &pages[i]);
-    }
-    LRU(pages, frames, time, fCount, pCount);
-
-    return 0;
-}
-
-void LRU(int pages[], int frames[], int time[], int fCount, int pCount) {
-    printf("\nRef.String   |\tFrames\n");
-    printf("-------------------------------\n");
-
-    int i, j, k, pos, flag, faultCount, counter, queue;
-    counter = 0, queue = 0, faultCount = 0;
-
-    for (i = 0; i < pCount; ++i) {
-        flag = 0;
-        printf("  %d\t|\t", pages[i]);
-        // check if already present
-        for (j = 0; j < fCount; ++j) {
-            if (frames[j] == pages[i]) {
-                flag = 1;                                   // this means that the character is already there in one of the frames.
-                counter++;
-                time[j] = counter;                          // update occurance
-                printf("   Hit\n\n");
-                break;
-            }
-        }
-        // push if there is free space
-        if ((flag == 0) && (queue < fCount)) {              // empty frames[] spaces
-            faultCount++;
-            counter++;
-            frames[queue] = pages[i];
-            time[queue] = counter;
-            queue++;                                        // queue is to keep the count of number of frames which have been filled.
-        }
-        // replacement as frames[] is full
-        else if ((flag == 0) && (queue == fCount)) {        // the frames are full now
-            faultCount++;
-            counter++;
-            pos = findLRU(time, fCount);
-            frames[pos] = pages[i];
-            time[pos] = counter;
-        }
-        // printing
-        if (flag == 0) {
-            for (k = 0; k < fCount; ++k) {
-                printf("%d  ", frames[k]);
-            }
-            printf("\n\n");
-        }
-    }
-    printf("Total Page Faults = %d\n\n", faultCount);
-}
-
-int findLRU(int time[], int fCount) {
-    int k, min, pos;
-    pos = 0;
-    min = time[0];
-    for (k = 1; k < fCount; ++k) {
-        if (time[k] < min) {
-            min = time[k];
-            pos = k;
-        }
-    }
-    return pos;
+#include<stdio.h>
+struct frames
+{
+	int content,count;
+}frame[100];
+void main()
+{
+	int i,j,k,p,f,page[100],cnt=1,min,pf=0,id=0;
+	printf("ENTER THE NUMBER OF PAGES : ");
+	scanf("%d",&p);
+	printf("ENTER THE REFERENCING STRING : ");
+	for(i=0;i<p;i++)
+	{
+		scanf("%d",&page[i]);
+	}
+	printf("ENTER THE NUMBER OF FRAMES : ");
+	scanf("%d",&f);
+	for(i=0;i<f;i++)
+	{
+		frame[i].content = -1;
+		frame[i].count = 0;
+	}
+	printf("\nREFERENCING PAGE\tSTATUS\t\tFRAME CONTENT\n\n");
+	for(i=0;i<p;i++)
+	{
+		printf("\t%d\t\t",page[i]);
+		for(j=0;j<f;j++)
+		{
+			if(frame[j].content == page[i])
+			{
+				printf("HIT\t\t");
+				frame[j].count = cnt++;
+				break;
+			}
+		}
+		if(j==f)
+		{
+			printf("MISS\t\t");
+			if(id<f)
+			{
+				frame[id].content = page[i];
+				frame[id].count = cnt++;
+				id++;
+			}
+			else
+			{
+				min=0;
+				for(k=0;k<f;k++)
+				{
+					if(frame[k].count<frame[min].count)
+						min = k;
+				}
+				frame[min].content = page[i];
+				frame[min].count = cnt++;
+			}
+			pf++;
+		}
+		for(j=0;j<f;j++)
+		{
+			if(frame[j].content !=-1)
+				printf("%d\t",frame[j].count);
+		}
+		printf("\n");
+	}
+	printf("\nPAGE FAULT : %d\n",pf);
 }
